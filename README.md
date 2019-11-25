94d94cae1d9584b804dd0afea6890f3e53e76326
Jen C <jenjch8@gmail.com>
2019-11-25 13:03:51 -0600

    made minor CSS edits and finished README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# dayPlanner
+# Homework 4 - Day Planner
+
+The aim of this assignment was to create a daily planner from 9 am to 5 pm, with ability to type in To Do's in each time slot, which will save to local storage and retain on the page. Each time slot will also be color-coded to reflect whether the time is in the past, current, or in the future.
+
+## Composition of Website and Code
+
+Whenever I could, I used jQuery to code the javascript. The HTML and CSS files were provided. I created a new javascript file. I didn't edit the HTML file (besides linking to my created javascript file and adding a header container) since I added HTML dynamically, and made some minor edits to the CSS file.
+
+## Process
+
+I first read through the moment.js documentation and googled some examples to understand how to utilize this in my javascript for displaying the current time in the header, as well as to later use for the dynamic color coding. 
+
+I did some pseudo coding to break down the steps for creating the rows/columns/classes/ids/data attributes, and for the onclick event/local storage.
+
+When writing out the jQuery code, it was continuous trial and error, and correction, when the code didn't manifest as expected. This helped me learn exactly what the code was doing. Console logging was also helpful when I couldn't easily (and visually) understand what the code was doing.
+
+During and after completing the assignment, I commented out each step of my javascript code, so that when I come back to it, I could understand exactly what I did and why. 
+
+## Deployment
+
+Aftering completing all requirements for the assignment, I did a final update of the files in Github and deployed the website for review. 
+https://github.com/jenjch/dayPlanner
+https://jenjch.github.io/dayPlanner
+
+## Acceptance Criteria
+
+Feedback on best practices, and improvements I could make would be helpful.
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
 // use moment to output current date (day of week, month in text, day with st/nd/rd/th, lower case h is 12 hour time with A for AM/PM)
 $(document).ready(function() {
   var momentDate = moment().format("dddd, MMMM Do, YYYY, h:mm A");
   // Use jquery instead of pure javascript
-    // var displayMomentDate = document.getElementById('currentDay');
-    // displayMomentDate.innerHTML = momentDate;
-  // Output moment info into existing id in HTML   
+  // var displayMomentDate = document.getElementById('currentDay');
+  // displayMomentDate.innerHTML = momentDate;
+
+  // Output moment info into existing id in HTML
   $("#currentDay").text(momentDate);
 
   // Add each time text from 9 am to 5 pm in array
@@ -21,7 +21,7 @@ $(document).ready(function() {
     "5PM"
   ];
 
-// Use for loop to add all divs, classes, ids, etc. (using some existing classes available in provided CSS)
+  // Use for loop to add all divs, classes, ids, etc. (using some existing classes available in provided CSS)
   for (var i = 0; i < dailyTimes.length; i++) {
     // Generated row div, added classes, and appended to container
     var rowDiv = $("<div>");
@@ -42,7 +42,7 @@ $(document).ready(function() {
     addTime.text(dailyTimes[i]);
     $(".row" + i).append(addTime);
 
-    // Generated/added text area to each row with set col and description class, and unique textAreaInput classes
+    // Generated/added text area to each row with set col and description class, and unique textInput classes
     var textArea = $("<textarea>");
     textArea.addClass("col-sm-10 description textInput" + i);
     $(".row" + i).append(textArea);
@@ -51,16 +51,16 @@ $(document).ready(function() {
     var addSaveButton = $("<button>");
     addSaveButton.addClass("saveBtn far fa-save col-sm-1");
     // Created ids for each button to use for later on click event
-    addSaveButton.attr("id", "textInput" + i)
+    addSaveButton.attr("id", "textInput" + i);
     $(".row" + i).append(addSaveButton);
   }
 
-// Created function to compare data attribute in row with moment.js to dynamically add color classes from CSS based on comparison of current moment time with each row's designated time.
+  // Created function to compare data attribute in row with moment.js to dynamically add color classes from CSS based on comparison of current moment time with each row's designated time.
   $(".row").each(function(index, element) {
     console.log(element);
 
     var pageTime = $(element).attr("data-time");
-    // k is 1-24 hours format 
+    // k is 1-24 hours format
     var momentTime = moment().format("k");
 
     if (pageTime === momentTime) {
@@ -72,27 +72,27 @@ $(document).ready(function() {
     }
   });
 
-  // Set up Save button click event to save to local storage. Used the Ids created for each individual button since jQuery only works on first of button of same class
+  // Set up button click event to save to local storage. Used the ids (textInputi) created for each individual button since jQuery only works on first of button of same class (saveBtn). Used class of same name as id to pull the text value.
   $(document).on("click", ".saveBtn", function() {
     console.log($(this).attr("id"));
+    // this refers to the button that was clicked
     var textInput = $(this).attr("id");
     var text = $("." + textInput).val();
     console.log(text);
-    // this refers to the button that was clicked
-    
-    // store based on key and value
+
+    // Store based on key and value
     localStorage.setItem(textInput, text);
+    alert("Saved!");
   });
 
-  // retrieve all objects in storage and output based on index, i.e. {textInput0: "inserted text"} in localStorage
+  // Retrieve all objects in storage and output based on index, i.e. {textInput0: "inserted text"} in localStorage
   var allStorage = Object.entries(localStorage);
-    // use for loop to account for all individual input fields.
-   for (var i=0; i < allStorage.length; i++) {
-       var pair = allStorage[i];
-       var key = pair[0];
-       var value = pair[1];
-    // Naming the Ids in the Button row and the classes in the textarea row the same allows us to "pass" the value to output/retain input from local storage onto page.
-       $("." + key).val(value);
-   }
-
+  // Used for loop to account for all individual input fields.
+  for (var i = 0; i < allStorage.length; i++) {
+    var pair = allStorage[i];
+    var key = pair[0];
+    var value = pair[1];
+    // Naming the ids in the Button row (originally used as key to store) and the classes in the textarea row the same allows us to "pass" the value to output/retain input from local storage onto page.
+    $("." + key).val(value);
+  }
 });
